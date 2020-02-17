7c510252158b86345e68844f4094d7c942b55c51
EthanAllison <60484481+EthanAllison@users.noreply.github.com>
2020-02-18 09:44:43 +1300

    Update Guess the number.py

--- Guess the number.py
+++ Guess the number.py
@@ -1,12 +1,13 @@
 """This is a program that guesses a number using while loops 17/2/2020"""
-#setting constants
+#Getting random number
 import random
 number = random.randint(1,10)
-
+#setting loop
 keep_asking = True
 while keep_asking == True:
+#Asking user to guess the number
     guess = int(input("Guess a number between 1 and 10\n"))
-
+#Checking wether it is correct or not 
     if guess == number:
         keep_asking = False
     elif guess >number:
@@ -14,5 +15,5 @@ while keep_asking == True:
     else:
         print("Your guess is to low, guess higher")
     
-
+#if they get it correct
 print("Correct the number is", number)
