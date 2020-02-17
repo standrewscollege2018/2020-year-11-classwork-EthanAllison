758b700a661fdd6241f2af2f7405f87cb786aca0
EthanAllison <60484481+EthanAllison@users.noreply.github.com>
2020-02-18 10:20:48 +1300

    Update Guess the number.py

--- Guess the number.py
+++ Guess the number.py
@@ -3,17 +3,21 @@
 import random
 number = random.randint(1,10)
 #setting loop
-keep_asking = True
-while keep_asking == True:
-#Asking user to guess the number
-    guess = int(input("Guess a number between 1 and 10\n"))
-#Checking wether it is correct or not 
-    if guess == number:
-        keep_asking = False
-    elif guess >number:
-        print("Your guess is to high, guess lower")
-    else:
-        print("Your guess is to low, guess higher")
-    
+error_catch = True
+while error_catch == True:
+#Asking user to guess the number and checking to see if they are correct
+#error catching
+    try:
+        guess = int(input("Guess a number between 1 and 10\n"))
+        if guess == number:
+            error_catch = False
+        elif guess >number:
+            print("Your guess is to high, guess lower")
+        else:
+            print("Your guess is to low, guess higher")
+            
+    except:
+        print("Error,Invalid number")
+        
 #if they get it correct
 print("Correct the number is", number)
